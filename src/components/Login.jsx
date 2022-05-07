8a506f3b08a99bee102601e73c08636386247ccb
Dan33311 <60445863+Dan33311@users.noreply.github.com>
2022-05-07 18:26:41 -0500

    Login, login validations sweetalert2

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,88 @@
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+const MySwal = withReactContent(Swal)
+
+
 const Login = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const email = e.target.email.value
+    const password = e.target.password.value
+
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+
+    // no empty fiels
+    if(email === "" || password === ""){
+      // MySwal.fire(<p>there is empty fields</p>)
+      Swal.fire({
+        title: 'There is empty fields!',
+        text: 'Do you want to continue',
+        icon: 'error',
+        confirmButtonText: 'Cool'
+      })
+      return
+    }
+
+    // REGEX
+    const regexEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    
+    if(email !== "" && !regexEmail.test(email)){
+      Swal.fire({
+        title: 'Invalid email format!',
+        text: 'Do you want to continue',
+        icon: 'error',
+        confirmButtonText: 'Cool'
+      })
+      return
+    }
+
+    // verify correct credentials email and password
+    if(email !== 'challenge@alkemy.org' && password !== 'react'){
+      Swal.fire({
+        title: 'Invalid credentials!',
+        text: 'Do you want to continue',
+        icon: 'error',
+        confirmButtonText: 'Cool'
+      })
+      return
+    }
+
+    Toast.fire({
+      icon: 'success',
+      title: 'Login successfully'
+    })
+
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
-          <span>Email:</span>
-          <input type="email" />
+          <span>Email:</span><br />
+          <input type="text" name="email"/>
         </label>
+        <br />
+
+        <label>
+          <span>Password:</span><br />
+          <input type="password" name="password"/>
+        </label>
+        <br />
+
+        <button>Enviar</button>
       </form>
     </>
   );
