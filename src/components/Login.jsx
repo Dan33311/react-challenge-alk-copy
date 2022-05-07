f1338eff2a86d94035072f16cc2803eb3f5d1474
Dan33311 <60445863+Dan33311@users.noreply.github.com>
2022-05-07 17:41:42 -0500

    first commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+const Login = () => {
+  return (
+    <>
+      <form>
+        <label>
+          <span>Email:</span>
+          <input type="email" />
+        </label>
+      </form>
+    </>
+  );
+}
+
+export default Login;
